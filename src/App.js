bf6662c4ae8b1284c360516ee7d64f048082664f
Chetan kumar <chetankumar71297@gmail.com>
2023-05-28 18:19:16 +0530

    Code General info component
    Components folder with three new files has been created in src folder.
    General-info.js file uses conditional rendering input fields and General
    info section of resume. conditional rendering depends upon which button
    among Submit and Edit has been clicked. By default it renders input
    fields. For these, button jsx with handleOnClick function is added to
    App.js file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import React from "react";
+import GeneralInfo from "./components/General-info";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      previewResume: false,
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick() {
+    const { previewResume } = this.state;
+    this.setState({ previewResume: !previewResume });
+  }
+  render() {
+    return (
+      <div className="App">
+        <GeneralInfo previewResume={this.state.previewResume} />
+        <button
+          onClick={() => {
+            this.handleOnClick();
+          }}
+        >
+          {this.state.previewResume ? "Edit" : "Submit"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/General-info.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+class GeneralInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      generalInfo: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        address: "",
+      },
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+  handleOnChange(event, field) {
+    const { generalInfo } = this.state;
+    generalInfo[field] = event.target.value;
+    this.setState({ generalInfo });
+  }
+  render() {
+    if (!this.props.previewResume) {
+      return (
+        <div>
+          <h1>General information</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            value={this.state.generalInfo.name}
+            onChange={(event) => {
+              this.handleOnChange(event, "name");
+            }}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            value={this.state.generalInfo.email}
+            onChange={(event) => {
+              this.handleOnChange(event, "email");
+            }}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            value={this.state.generalInfo.phoneNumber}
+            onChange={(event) => {
+              this.handleOnChange(event, "phoneNumber");
+            }}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Address"
+            value={this.state.generalInfo.address}
+            onChange={(event) => {
+              this.handleOnChange(event, "address");
+            }}
+          />
+          <br />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Name: {this.state.generalInfo.name}</p>
+          <p>Email: {this.state.generalInfo.email}</p>
+          <p>Phone Number: {this.state.generalInfo.phoneNumber}</p>
+          <p>Address: {this.state.generalInfo.address}</p>
+        </div>
+      );
+    }
+  }
+}
+
+export default GeneralInfo;
